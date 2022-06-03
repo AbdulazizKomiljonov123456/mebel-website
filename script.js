6cbf7f8c927598f452495bd1b232ee20d2c8e962
abdulaziz <abdulazi@gmail.com>
2022-06-03 18:47:21 +0300

    mebel

---
+++ script.js
@@ -0,0 +1,14 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-XF546HDME2');
+
+function closeModalFunc() {
+    var MobileModal = document.getElementById('mobileModal')
+    MobileModal.classList.add("d-none");
+}
